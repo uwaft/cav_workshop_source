dfeb05ead4314b8afe471a866aac1d6658d1f0f8
Pranay <p5nagpal@uwaterloo.ca>
2018-11-22 16:12:28 -0500

    added new sensor, changed road config, and added a new plot (more desc in file

--- SensorFusionUsingSyntheticRadarandVisionDataWorkshop.m
+++ SensorFusionUsingSyntheticRadarandVisionDataWorkshop.m
@@ -1,28 +1,135 @@
+% NOTE: Added a camera on the front right side of the (left-side-steering) car. Has 2 benefits:
+% 1. Useful while turning right on a roundabout as shown in the scenerio
+% (which can be a difficult maneuver if there's a passenger in the front
+% seat)
+% 2. Also useful in trucks and bigger vehicles which have that area as a
+% blindspot.
+% Remark: THe came would be added on the left side for a righ-side-steering
+% vehicle.
+
+
 % Define an empty scenario.
 scenario = drivingScenario;
-scenario.SampleTime = 0.01;
+%scenario.SampleTime = 0.01;
 
-roadCenters = [0 0; 50 0; 100 0; 250 20; 500 40];
-road(scenario, roadCenters, 'lanes',lanespec(2));
+%roadCenters = [0 0; 50 0; 100 0; 250 20; 500 40];
+%road(scenario, roadCenters, 'lanes',lanespec(2));
 
-% Create the ego vehicle that travels at 25 m/s along the road.  Place the
-% vehicle on the right lane by subtracting off half a lane width (1.8 m)
-% from the centerline of the road.
-egoCar = vehicle(scenario, 'ClassID', 1);
-path(egoCar, roadCenters(2:end,:) - [0 1.8], 25); % On right lane
+% Add all road segments
+roadCenters = [-10.9 -3.5 0;
+    26.1 -3.2 0;
+    50.9 -3.1 0];
+laneSpecification = lanespec(2, 'Width', 8);
+road(scenario, roadCenters, 'Lanes', laneSpecification);
 
-% Add a car in front of the ego vehicle
-leadCar = vehicle(scenario, 'ClassID', 1);
-path(leadCar, [70 0; roadCenters(3:end,:)] - [0 1.8], 25); % On right lane
+roadCenters = [25.6 29.3 0;
+    26.2 -50.7 0];
+laneSpecification = lanespec(2, 'Width', 8);
+road(scenario, roadCenters, 'Lanes', laneSpecification);
 
-% Add a car that travels at 35 m/s along the road and passes the ego vehicle
-passingCar = vehicle(scenario, 'ClassID', 1);
-waypoints = [0 -1.8; 50 1.8; 100 1.8; 250 21.8; 400 32.2; 500 38.2];
-path(passingCar, waypoints, 35);
+% Add the ego car
+egoCar = vehicle(scenario, ...
+    'ClassID', 1, ...
+    'Position', [-8.1 -7.4 0]);
+waypoints = [-8.1 -7.4 0;
+    5.1 -7.2 0;
+    9.5 -7 0;
+    13.1 -6.9 0;
+    15.6 -6.9 0;
+    17.7 -6.7 0;
+    20.5 -7.7 0;
+    23 -9 0;
+    25.1 -9.2 0;
+    28.4 -8.3 0;
+    31.3 -6.3 0;
+    31.1 -0.5 0;
+    29.2 1.5 0;
+    29.2 4.5 0;
+    29.4 8.3 0;
+    29.5 13.3 0;
+    29.5 19.9 0;
+    29.7 26.8 0];
+speed = 35;
+trajectory(egoCar, waypoints, speed);
 
-% Add a car behind the ego vehicle
-chaseCar = vehicle(scenario, 'ClassID', 1);
-path(chaseCar, [25 0; roadCenters(1:end,:)] - [0 1.8], 25); % On right lane
+% Add the non-ego actors
+car1 = vehicle(scenario, ...
+    'ClassID', 1, ...
+    'Position', [29.9 -48 0]);
+waypoints = [29.9 -48 0;
+    30.1 -29.9 0;
+    30.1 -25.2 0;
+    30.1 -18.4 0;
+    29.9 -13.4 0;
+    29.9 -11.4 0;
+    31.3 -8.5 0;
+    32 -3.2 0;
+    30.9 1 0;
+    29.5 2.6 0;
+    29 5.1 0;
+    29.4 9.2 0;
+    29.5 14 0];
+speed = 25;
+trajectory(car1, waypoints, speed);
+
+% Creating the roundabout using barriers
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [25.5 -0.4 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [23.5 -1.7 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [29.2 -3.2 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [27.6 -1.8 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [23.5 -4.9 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [25.5 -6 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [27.8 -4.8 0]);
+
+actor(scenario, ...
+    'ClassID', 5, ...
+    'Length', 2.4, ...
+    'Width', 0.76, ...
+    'Height', 0.8, ...
+    'Position', [22.7 -3.2 0]);
+
+
 
 sensors = cell(8,1);
 % Front-facing long-range radar sensor at the center of the front bumper of the car.
@@ -37,7 +144,7 @@ sensors{2} = radarDetectionGenerator('SensorIndex', 2, 'Height', 0.2, 'Yaw', 180
 sensors{3} = radarDetectionGenerator('SensorIndex', 3, 'Height', 0.2, 'Yaw', 120, ...
     'SensorLocation', [0, egoCar.Width/2], 'MaxRange', 30, 'ReferenceRange', 50, ...
     'FieldOfView', [90, 5], 'AzimuthResolution', 10, 'RangeResolution', 1.25);
-
+% 
 % Rear-right-facing short-range radar sensor at the right rear wheel well of the car.
 sensors{4} = radarDetectionGenerator('SensorIndex', 4, 'Height', 0.2, 'Yaw', -120, ...
     'SensorLocation', [0, -egoCar.Width/2], 'MaxRange', 30, 'ReferenceRange', 50, ...
@@ -58,6 +165,11 @@ sensors{6} = radarDetectionGenerator('SensorIndex', 6, 'Height', 0.2, 'Yaw', -60
 % Front-facing camera located at front windshield.
 sensors{7} = visionDetectionGenerator('SensorIndex', 7, 'FalsePositivesPerImage', 0.1, ...
     'SensorLocation', [0.75*egoCar.Wheelbase 0], 'Height', 1.1);
+
+% Right-side-facing camera located near the right back-view mirror.
+sensors{9} = visionDetectionGenerator('SensorIndex', 9, 'FalsePositivesPerImage', 0.1, ...
+    'SensorLocation', [egoCar.Wheelbase, -egoCar.Width/2], 'Height', 0.2, 'Yaw', -60,  ... 
+     'MaxRange', 30);
 
 % Rear-facing camera located at rear windshield.
 sensors{8} = visionDetectionGenerator('SensorIndex', 8, 'FalsePositivesPerImage', 0.1, ...
@@ -82,8 +194,8 @@ while advance(scenario) && ishghandle(BEP.Parent)
 
     % Simulate the sensors
     detections = {};
-    isValidTime = false(1,8);
-    for i = 1:8
+    isValidTime = false(1,9);
+    for i = 1:9
         [sensorDets,numValidDets,isValidTime(i)] = sensors{i}(ta, time);
         if numValidDets
             for j = 1:numValidDets
@@ -180,13 +292,20 @@ function BEP = createDemoDisplay(egoCar, sensors)
     hCarPlot = axes(hCarViewPanel);
     chasePlot(egoCar, 'Parent', hCarPlot);
 
+    % Add a car plot that follows the ego vehicle from left
+    hLeftViewPanel = uipanel(hFigure, 'Position', [0.5 0 0.5 0.5], 'Title', 'Left Camera View');
+    hCarPlot = axes(hLeftViewPanel);
+    chasePlot(egoCar, 'Parent', hCarPlot,'ViewHeight', 3, 'ViewLocation', [0 10], 'ViewYaw', -67);
+    
     % Add a car plot that follows the ego vehicle from a top view
     hTopViewPanel = uipanel(hFigure, 'Position', [0 0.5 0.5 0.5], 'Title', 'Top View');
     hCarPlot = axes(hTopViewPanel);
     chasePlot(egoCar, 'Parent', hCarPlot, 'ViewHeight', 130, 'ViewLocation', [0 0], 'ViewPitch', 90);
 
+    
+    
     % Add a panel for a bird's-eye plot
-    hBEVPanel = uipanel(hFigure, 'Position', [0.5 0 0.5 1], 'Title', 'Bird''s-Eye Plot');
+    hBEVPanel = uipanel(hFigure, 'Position', [0.5 0.5 0.5 0.5], 'Title', 'Bird''s-Eye Plot');
 
     % Create bird's-eye plot for the ego car and sensor coverage
     hBEVPlot = axes(hBEVPanel);
@@ -201,7 +320,7 @@ function BEP = createDemoDisplay(egoCar, sensors)
     end
 
     % Plot the coverage areas for vision sensors
-    for i = 7:8
+    for i = 7:9
         cap = coverageAreaPlotter(BEP,'FaceColor','blue','EdgeColor','blue');
         plotCoverageArea(cap, sensors{i}.SensorLocation,...
             sensors{i}.MaxRange, sensors{i}.Yaw, 45);
